6e3cfc14e175c8fe84c2335173b40309225fa914
Emiliyan Stefkov <93265496+EStefkov@users.noreply.github.com>
2024-06-02 19:34:03 +0300

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,14 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-     
-    },
+    container:{
+      center:true,
+      padding:{
+        DEFAULT: "1rem",
+        md:"1.5rem",
+        lg:"2rem"
+    }
+  }
   },
   plugins: [],
 };
